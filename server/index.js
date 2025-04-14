31452f9e3cc8a512f008d0b0b31ea25630f35906
alexander-john <96aljohn@gmail.com>
2025-04-14 00:31:40 -0700

    Add db.js file. Refactor MongoDB connection.
    There was an opportunity to refactor the MongoDB connection for a more modular approach and to improve code structure.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 // Load environment variables from .env files based on NODE_ENV (default to .env.development)
 const dotenv = require('dotenv');
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 // Import custom route handlers
 const questionRoutes = require('./routes/questionRoutes');
 const codeRoute = require('./routes/codeRoutes');
+
+// Import configuration utilities
+const connectDB = require('./config/db');
 
 // Determine the environment and load corresponding .env file
 const env = process.env.NODE_ENV || 'development';
@@ -27,21 +29,21 @@ app.use(cors(corsOptions));
 // Parse incoming JSON requests
 app.use(express.json());
 
-// Connect to MongoDB using Mongoose
-(async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected');
-    } catch (err) {
+// Connect to MongoDB
+connectDB()
+    .then(() => {
+        // Use question and code API routes
+        app.use('/api/questions', questionRoutes); // Handles question-related routes
+        app.use('/api/code', codeRoute); // Handles code submission and evaluation
+
+        // Start the Express server on the specified port
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+    })
+    .catch((err) => {
         console.error('MongoDB connection error:', err.message);
         process.exit(1);
-    }
-})();
+    });
 
-// Use question and code API routes
-app.use('/api/questions', questionRoutes); // Handles question-related routes
-app.use('/api/code', codeRoute); // Handles code submission and evaluation
 
-// Start the Express server on the specified port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
